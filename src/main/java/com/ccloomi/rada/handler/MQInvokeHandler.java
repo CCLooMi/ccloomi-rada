bad55e5b5728a4d4fd74148a05431aa74472062b
Seemie <1004178387@qq.com>
2020-05-12 15:59:59 +0800

    修复RPC调用时Map转Map等带泛型的转换问题(两个map有不同的key和value类型)

--- src/main/java/com/ccloomi/rada/handler/MQInvokeHandler.java
+++ src/main/java/com/ccloomi/rada/handler/MQInvokeHandler.java
@@ -1,8 +1,11 @@
 package com.ccloomi.rada.handler;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 
 /**© 2015-2017 CCLooMi.Inc Copyright
  * 类    名：MDHandler
@@ -14,17 +17,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class MQInvokeHandler {
 	private Method method;
 	private Object target;
-	private Class<?>[]pTypes;
+	private JavaType[]types;
 	public MQInvokeHandler(Method method,Object target) {
 		this.setMethod(method);
 		this.setTarget(target);
 	}
 	public Object execute(Object[]args) throws Exception{
-		return method.invoke(target, args);
+		if(types.length>0) {
+			return method.invoke(target, args);
+		}
+		return method.invoke(target, new Object[0]);
 	}
 	public Object execute(ObjectMapper om,Object[]args) throws Exception{
-		for(int i=0;i<args.length&&i<pTypes.length;i++) {
-			args[i]=om.convertValue(args[i], pTypes[i]);
+		if(types.length>0) {
+			for(int i=0;i<args.length&&i<types.length;i++) {
+				args[i]=om.convertValue(args[i], types[i]);
+			}
 		}
 		return execute(args);
 	}
@@ -36,7 +44,15 @@ public class MQInvokeHandler {
 	public void setMethod(Method method) {
 		this.method = method;
 		this.method.setAccessible(true);
-		this.pTypes=method.getParameterTypes();
+
+		if(method.getParameterCount()>0) {
+			TypeFactory typeFactory=TypeFactory.defaultInstance();
+			Parameter[]ps=method.getParameters();
+			this.types=new JavaType[ps.length];
+			for(int i=0;i<ps.length;i++) {
+				types[i]=typeFactory.constructType(ps[i].getParameterizedType());
+			}
+		}
 	}
 	/**获取 target*/
 	public Object getTarget() {
